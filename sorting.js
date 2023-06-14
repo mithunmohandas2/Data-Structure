131986fafe974f13a99b17f4726b1abbd5655031
MITHUN MOHANDAS <mithunsruthi@gmail.com>
2023-06-14 17:58:26 +0530

    Insertion sort added

--- sorting.js
+++ sorting.js
@@ -1,5 +1,3 @@
-const array = [8,0, -7, 5, 4, -6, 8, 2, 4, -1]
-
 //Bubble Sort Method
 
 function bubbleSort(array) {
@@ -17,9 +15,29 @@ function bubbleSort(array) {
     }
     return array;
 }
-
-
-// console.log(bubbleSort(array))    
+    
 
 // ====================================
 
+// Insertion Sort
+
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let check = array[i];
+        let j = i - 1;
+        while (j >= 0 && check < array[j]) {
+            array[j + 1] = array[j]
+            j = (j - 1)
+        }
+        array[j + 1] = check
+    }
+    return array
+}
+
+// ======================================
+//Sorting Methods:
+
+const array = [8,0, -7, 5, 4, -6, 8, 2, 4, -1]
+
+// console.log(bubbleSort(array)) 
+console.log(insertionSort(array))  
