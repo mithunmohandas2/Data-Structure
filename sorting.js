4a93a4c441d8862f7ad8186eb96aa4ba7297dc95
MITHUN MOHANDAS <mithunsruthi@gmail.com>
2023-06-15 13:10:46 +0530

    Selection Sort added

--- sorting.js
+++ sorting.js
@@ -39,24 +39,41 @@ function insertionSort(array) {
 
 // Quick Sort:
 function quickSort(array) {
-    if (array.length<=1) return array;
-    let left=[];
-    let right=[];
-    const pivot=array[array.length-1]    //taking lase element as pivot
-    for (let i = 0; i < array.length-1; i++) {
-        if (array[i]<pivot)left.push(array[i])
-        else if (array[i]>=pivot)right.push(array[i])
+    if (array.length <= 1) return array;
+    let left = [];
+    let right = [];
+    const pivot = array[array.length - 1]    //taking lase element as pivot
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] < pivot) left.push(array[i])
+        else if (array[i] >= pivot) right.push(array[i])
     }
-    return  [...quickSort(left),pivot,...quickSort(right)]
+    return [...quickSort(left), pivot, ...quickSort(right)]
 }
 
+// ====================================
 
+// Selection Sort:
+
+const array = [66,-2, -267, 776, 9, 31, -213, 152, -349]
+function selectionSort(array) {
+    for (let i = 0; i < array.length - 1; i++)
+        {
+            for (let j=i+1; j<array.length;j++){
+                if (array[i]<array[j]){    // descending order
+                    let temp = array[i]
+                    array[i]= array[j]
+                    array[j]= temp
+                }
+            }
+        }
+       return array 
+}
 
 // ======================================
 //Sorting Methods:
 
-const array = [0,22,-29,445,-9099,1,13,12,-9]
 
 // console.log(bubbleSort(array))
-// console.log(insertionSort(array))  
-console.log(quickSort(array))
+// console.log(insertionSort(array))
+// console.log(quickSort(array))
+console.log(selectionSort(array))
